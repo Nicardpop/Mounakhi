f787d18e6b12d6757bcbbeac3b6c48dcc7736290
Hac_pop_nicard <92998191+Nicardpop@users.noreply.github.com>
2025-01-06 03:15:46 +0100

    added lon&lat and fixed the icon issue

--- index.js
+++ index.js
@@ -13,8 +13,11 @@ let wind = document.querySelector('.wind');
 let icon = document.getElementById('wicon');
 let main = document.querySelector('displayWeather');
 
+let lati = document.querySelector('.lat');
+let long = document.querySelector('.lon');
+
 (()=>{
-    main.innerHTML = "";
+    main.innerHTML = "Waiting ...";
    
 })
 
@@ -46,8 +49,7 @@ geobutton.addEventListener('click', function(){
     
 
 })
-
-button.addEventListener('click', function(){
+function click(){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputvalue.value}&units=metric&appid=108dd9a67c96f23039937fe6f3c91963`)
     .then(response =>{
         if (response.status == 200){
@@ -66,7 +68,12 @@ button.addEventListener('click', function(){
         
     ); 
 
-})
+}
+button.addEventListener('click', click)
+// for detecting the "enter" key press 
+inputvalue.addEventListener('keydown',(event) => {if(event.key === 'Enter'){
+    click()
+}});
 
 const displayData=(weather)=>{
     var iconcode = weather.weather[0].icon;
@@ -74,12 +81,20 @@ const displayData=(weather)=>{
     var cty = weather.name;
     countryName.innerText = `Country : ${cty}`;
 
-    let temperature = weather.main.temp;
+    let temperature = Math.round(weather.main.temp);
     let description = weather.weather[0].description;
     let windSpeed = Math.round(weather.wind.speed);
     let humdity = Math.round(weather.main.humidity);
 
+    let longitude = weather.coord.lon;
+    let latitude = weather.coord.lat;
+    console.log(longitude);
+    console.log(latitude);
+
     var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    lati.innerText = `latitude : ${latitude}`
+    long.innerText = `longitude : ${longitude}`
+
     icon.src = iconurl;
     temp.innerText=`${temperature}°C`
     desc.innerText=`${description}`
